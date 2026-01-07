38344e577482bd92469222e9e1690001d39db7dc
clinton-tpc <clinton@tpc.training>
2026-01-07 19:40:42 +0200

    ready for ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -243,6 +243,13 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4896993903038581"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body className="font-sans">
         <GoogleAnalytics />
